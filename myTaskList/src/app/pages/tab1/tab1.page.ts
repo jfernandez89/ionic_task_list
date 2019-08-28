442ac38448ab8483efc23c65c979e630a6eef18c
javier Fernandez <jfernandez.developer@gmail.com>
2019-08-28 20:59:08 +0200

    Added an ion-fab to add elements, a new component and routes

--- myTaskList/src/app/pages/tab1/tab1.page.ts
+++ myTaskList/src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { WishesService } from '../../services/wishes.service';
-import { List } from '../../models/list.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -11,7 +11,12 @@ export class Tab1Page {
 
   tabName: string = 'Pendientes';
 
-  constructor(public wishes: WishesService) {
+  constructor(public wishes: WishesService, private _router: Router) {
 
+  }
+
+  addList(){
+    //Navigates from the index
+    this._router.navigateByUrl('/tabs/tab1/add');
   }
 }
