5991388596fcc47aa7c634875a99a82f101cf605
javier Fernandez <jfernandez.developer@gmail.com>
2019-12-14 13:51:24 +0100

    Added an icon to show an alert, that allows edit the title list

--- myTaskList/src/app/components/list/list.component.ts
+++ myTaskList/src/app/components/list/list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { WishesService } from '../../services/wishes.service';
 import { Router } from '@angular/router';
 import { List } from 'src/app/models/list.model';
+import { AlertController, IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -10,9 +11,11 @@ import { List } from 'src/app/models/list.model';
 })
 export class ListComponent implements OnInit {
 
+  //Get the only IonList available in the view, if there was more than one, we must add an id element to get it
+  @ViewChild(IonList) myIonList :IonList;
   @Input() finishedFrom = true;
 
-  constructor(public wishes: WishesService, private _router: Router) { }
+  constructor(public wishes: WishesService, private _router: Router, private _alertCtrl: AlertController) { }
 
   ngOnInit() { }
 
@@ -27,5 +30,44 @@ export class ListComponent implements OnInit {
 
   deleteList(list: List) {
     this.wishes.deleteList(list);
+  }
+
+  async editTitleList(list: List) {
+
+    const alert = await this._alertCtrl.create({
+      header: 'Editar título',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          value: list.title,
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+            this.myIonList.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Modificar',
+          handler: (data) => {
+            
+            if (data.title.length === 0) {
+              return;
+            }
+
+            list.title = data.title;
+            this.wishes.saveStorage();
+            this.myIonList.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
